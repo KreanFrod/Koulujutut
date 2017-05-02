9d50fa41f79f43229ed0af0c5e0400944af9d3b7
KreanFrod <kreanfrod@gmail.com>
2017-05-02 18:46:17 +0200

    Tässä on taas joitain muutoksia

--- Harjoitukset2/src/harjoitukset2/Matriisilaskin.java
+++ Harjoitukset2/src/harjoitukset2/Matriisilaskin.java
@@ -5,18 +5,39 @@
  */
 package harjoitukset2;
 
+import harjoitukset2.Matriisi;
 /**
  *
  * @author Marika
  */
 public class Matriisilaskin {
-boolean tarkistus = true;
-    public int[][] tarkistaMatriisit(int[][] ekamatriisi, int[][] tokamatriisi) {
-        if (ekamatriisi.length != tokamatriisi.length) {
+
+    
+// Tässä tarkistetaan, että annetut matriisit ovat saman suuruisia
+//public boolean tarkistaMatriisit(int[][] eka, int[][] toka) {
+//        if (eka.length != toka.length) {
+//            System.out.println("Matriisit ovat erikokoiset! Laskua ei voi suorittaa");
+//            tarkistus = false;
+//        } else {
+//            tarkistus = true;
+//        }
+//        return tarkistus;
+//    }
+// Tässä lasketaan kaksi matriisia yhteen
+
+    public int[][] summaMatriisi(int[][] eka, int[][] toka) {
+        if (eka.length != toka.length) {
             System.out.println("Matriisit ovat erikokoiset! Laskua ei voi suorittaa");
-            tarkistus = false;
-        } else {
-            tarkistus = true;
+            } else {
+            int a = eka.length;
+            int b = toka.length;
+            Matriisi tulos = new int[a][b];
+            for (int x = 0; x < a; ++x) {
+                for (int y = 0; y < tulos[x].length; ++y) {
+                    tulos[x][y] = eka[x][y] + toka[x][y];
+                }
+            }
         }
+    return tulos;
     }
 }
