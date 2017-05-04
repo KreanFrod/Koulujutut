c5304c281fa8e5e738c5c9097627e51fcfd1a898
KreanFrod <kreanfrod@gmail.com>
2017-05-04 19:04:37 +0200

    Maven harkat

---
+++ v8/src/test/java/com/v8/app/TestApp2.java
@@ -0,0 +1,16 @@
+package com.v8.app;
+
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class TestApp2 {
+
+    @Test
+    public void testPrintHelloWorld2() {
+
+        Assert.assertEquals(App.getHelloWorld2(), "Hello World 2");
+
+    }
+
+}
